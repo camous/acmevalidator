3bea4a96d058b06e693e782087534c78f72a9408
Fabien Camous <fabien.camous@bearingpoint.com>
2019-05-02 08:30:12 +0200

    bump to version 2.2.0

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rule = JObject.Parse(System.IO.File.ReadAllText("json1.json"));
+            var json = JObject.Parse(System.IO.File.ReadAllText("json2.json"));
+
+            var equals = new acmevalidator.Validator().Validate(json, rule, out Dictionary<JToken,JToken> delta);
+        }
+    }
+}
